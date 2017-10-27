f13c188ddecf0a7c015f4a9cad8f3a3df1e32ebc
Unknown <t.kodera.nichibi@gmail.com>
2017-10-27 14:16:14 +0900

    メディア追加・基本テンプレート

--- AideA_support/AlphabetCell.swift
+++ AideA_support/AlphabetCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class AlphabetCell: UICollectionViewCell {
 
+    @IBOutlet weak var lblAlphabet: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
---
+++ AideA_support/DictionaryViewController.swift
@@ -0,0 +1,56 @@
+//
+//  DictionaryViewController.swift
+//  AideA_master
+//
+//  Created by AppCircle on 2017/10/26.
+//  Copyright © 2017年 NichibiAppCircle. All rights reserved.
+//
+
+import UIKit
+
+class DictionaryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate
+{
+    @IBOutlet weak var alphabetsCollectionView: UICollectionView!
+    
+    var tests = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "z", "_", "%"]
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let nibAlphabet = UINib(nibName: "AlphabetCell", bundle: nil)
+        self.alphabetsCollectionView.register(nibAlphabet, forCellWithReuseIdentifier: "alphabetCell")
+        
+        self.alphabetsCollectionView.backgroundColor = UIColor.clear
+        
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return self.tests.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "alphabetCell", for: indexPath) as! AlphabetCell
+        
+        cell.lblAlphabet.text = self.tests[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
