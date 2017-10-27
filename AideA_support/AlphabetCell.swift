d8c49a6947e6fd39ee7633c2e9652a0222341474
Unknown <t.kodera.nichibi@gmail.com>
2017-10-27 10:49:28 +0900

    Initial commit

---
+++ AideA_support/AlphabetCell.swift
@@ -0,0 +1,18 @@
+//
+//  AlphabetCell.swift
+//  AideA_support
+//
+//  Created by AppCircle on 2017/10/27.
+//  Copyright © 2017年 NichibiAppCircle. All rights reserved.
+//
+
+import UIKit
+
+class AlphabetCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+AideA_support
